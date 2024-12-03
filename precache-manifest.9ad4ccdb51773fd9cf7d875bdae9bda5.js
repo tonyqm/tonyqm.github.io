7436246e688bb3b6ea3b4e7e79b9056d0240f5bf
Tony Meng <tqmeng@ucsd.edu>
2024-12-03 15:12:22 -0800

    Update calendar, and slight wording on about page

---
+++ precache-manifest.9ad4ccdb51773fd9cf7d875bdae9bda5.js
@@ -0,0 +1,458 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8aa5d1b6bc04a1c24358ca507980473",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617a427e679ddc9b5d31",
+    "url": "/static/css/main.e27c92dc.chunk.css"
+  },
+  {
+    "revision": "1188f3f8889f3223e88d",
+    "url": "/static/js/2.c6cfc82e.chunk.js"
+  },
+  {
+    "revision": "617a427e679ddc9b5d31",
+    "url": "/static/js/main.3906ffed.chunk.js"
+  },
+  {
+    "revision": "acd1850de325f8b4e567",
+    "url": "/static/js/runtime-main.22c07be9.js"
+  },
+  {
+    "revision": "4c533b712a560fc5c91ff0e7c4e9ab5e",
+    "url": "/static/media/AD1.4c533b71.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e1d4401abb776c94333c8701b7ffc0c8",
+    "url": "/static/media/ArrowScroll.e1d4401a.svg"
+  },
+  {
+    "revision": "54372af025ae40d389a64a0206780b46",
+    "url": "/static/media/CSlogo.54372af0.png"
+  },
+  {
+    "revision": "75c305db2917e692aa644a9fbb844919",
+    "url": "/static/media/Cover.75c305db.png"
+  },
+  {
+    "revision": "2080c687bb504101eebddddf2b325443",
+    "url": "/static/media/CurrentData.2080c687.png"
+  },
+  {
+    "revision": "b9af102d91b2df250270c69f0682d868",
+    "url": "/static/media/Dashboard.b9af102d.png"
+  },
+  {
+    "revision": "7009b45f33a04c8322023f18ae66b59c",
+    "url": "/static/media/DesignProcess.7009b45f.png"
+  },
+  {
+    "revision": "7572656217c338a2aa7cb1d7ebe42c1f",
+    "url": "/static/media/Develop.75726562.svg"
+  },
+  {
+    "revision": "4be966a102c014674069357169fd7480",
+    "url": "/static/media/Gmails.4be966a1.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "f1bca3929e3dca64163d1087e52b3a79",
+    "url": "/static/media/Home.f1bca392.png"
+  },
+  {
+    "revision": "8da794ba34ef79ea749ce347e9e2b499",
+    "url": "/static/media/HotIdeas.8da794ba.png"
+  },
+  {
+    "revision": "b320b12b5b82f0e7ab1a5bd0e3e6675c",
+    "url": "/static/media/Indie_Logo.b320b12b.jpeg"
+  },
+  {
+    "revision": "37033e6d943db9b1fd6229563e940239",
+    "url": "/static/media/Journal.37033e6d.png"
+  },
+  {
+    "revision": "622db6363ccb2f74e01d5bce2652c721",
+    "url": "/static/media/Landing.622db636.png"
+  },
+  {
+    "revision": "8c19a06391b367eec6cad3872888f227",
+    "url": "/static/media/Launch.8c19a063.svg"
+  },
+  {
+    "revision": "ed86af2ed5bbaf879e9f2ec2e2eac929",
+    "url": "/static/media/Montserrat-Bold.ed86af2e.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5e5332c70e62445ee877af67e702bf55",
+    "url": "/static/media/Original.5e5332c7.png"
+  },
+  {
+    "revision": "e0cd0525bd948661bc5a2f502f3ee0a8",
+    "url": "/static/media/Overview.e0cd0525.png"
+  },
+  {
+    "revision": "fc88fbe4eeeb32fa363f620a8a3aae7e",
+    "url": "/static/media/Overview.fc88fbe4.png"
+  },
+  {
+    "revision": "673015a61769958bf66e3f4c51d17df7",
+    "url": "/static/media/Personas.673015a6.png"
+  },
+  {
+    "revision": "98bdc723001c4cf3f4a7fafa1f165002",
+    "url": "/static/media/Plan.98bdc723.svg"
+  },
+  {
+    "revision": "1e8bf3e63e464eafac8b601c65d4834a",
+    "url": "/static/media/Profile.1e8bf3e6.png"
+  },
+  {
+    "revision": "408c8ebe3049435eb2ac1210fbfd3793",
+    "url": "/static/media/Prototype.408c8ebe.svg"
+  },
+  {
+    "revision": "e82be8d24e8cbc8406fd30af5c38fd8e",
+    "url": "/static/media/PublicRelationsImg.e82be8d2.svg"
+  },
+  {
+    "revision": "40be9a5c79ffa7c796b57cc6b0ddb8f6",
+    "url": "/static/media/Research.40be9a5c.svg"
+  },
+  {
+    "revision": "6da5776ce54c66b074bf0f0d989cde60",
+    "url": "/static/media/Resume.6da5776c.png"
+  },
+  {
+    "revision": "88f42816a968caa0c1cb48337004b6ef",
+    "url": "/static/media/Sketch.88f42816.svg"
+  },
+  {
+    "revision": "71273c90650ce0791ee92e201aac90c7",
+    "url": "/static/media/Sketching.71273c90.svg"
+  },
+  {
+    "revision": "f64daecdc923ea1915dbb4b3481e02e6",
+    "url": "/static/media/Submenus.f64daecd.png"
+  },
+  {
+    "revision": "0219f6c58e74871cecb2a33d7205bf7f",
+    "url": "/static/media/TaskMap.0219f6c5.png"
+  },
+  {
+    "revision": "e4b3f26752c7a4b6425a000e8587eae1",
+    "url": "/static/media/Taskathon User Flow.e4b3f267.png"
+  },
+  {
+    "revision": "728a0bbe428618b0a4bf4a37f75fd909",
+    "url": "/static/media/TaskathonBanner.728a0bbe.png"
+  },
+  {
+    "revision": "871f46e43d04edd9605081e6f89c44ed",
+    "url": "/static/media/Tasks.871f46e4.png"
+  },
+  {
+    "revision": "cb2ae2382170486009456562aa61227a",
+    "url": "/static/media/Technologies.cb2ae238.png"
+  },
+  {
+    "revision": "edcf8afe38b6968bf3eff3bb3f82925f",
+    "url": "/static/media/UXFlows.edcf8afe.png"
+  },
+  {
+    "revision": "60d107576c751d4f5208bebe6c4d3e9b",
+    "url": "/static/media/WeatherBanner.60d10757.png"
+  },
+  {
+    "revision": "6da9345c69f69ef19c234337d53f3d60",
+    "url": "/static/media/WeatherHub User Flow.6da9345c.png"
+  },
+  {
+    "revision": "69333f8f377388324613f5908f50d98d",
+    "url": "/static/media/Wireframes.69333f8f.png"
+  },
+  {
+    "revision": "390f21aee6f598e3500bcaa6f58a699a",
+    "url": "/static/media/ZillowBanner.390f21ae.png"
+  },
+  {
+    "revision": "bf750947b768d10e124b961a88aff79f",
+    "url": "/static/media/about_page_portrait.bf750947.jpg"
+  },
+  {
+    "revision": "3b021f8ffcf9f323ea72abc28382e234",
+    "url": "/static/media/address_image.3b021f8f.svg"
+  },
+  {
+    "revision": "52e0714e5ea7e3b96f50dbfb62de21d1",
+    "url": "/static/media/animated_logo.52e0714e.svg"
+  },
+  {
+    "revision": "05497ef7b427f9ea408a268ae3d72be8",
+    "url": "/static/media/appleIOS.05497ef7.svg"
+  },
+  {
+    "revision": "b6b12c7d07e0569e771c8aedf81a7767",
+    "url": "/static/media/blogs_image.b6b12c7d.svg"
+  },
+  {
+    "revision": "ded3f06b3422bf8beabe81ac7e3ae554",
+    "url": "/static/media/bloxtopiaIcon.ded3f06b.png"
+  },
+  {
+    "revision": "768635302f6b97f0de45a7d8c981ec46",
+    "url": "/static/media/city.76863530.png"
+  },
+  {
+    "revision": "dfcb7a585547c0b125d208b980d6f57f",
+    "url": "/static/media/cloud_infrastructure.dfcb7a58.svg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "52af8138c22deedbe0004ae0f9911db2",
+    "url": "/static/media/dataVisualization.52af8138.png"
+  },
+  {
+    "revision": "434d2b012c3efc3da1ef90549a50f46b",
+    "url": "/static/media/data_science.434d2b01.svg"
+  },
+  {
+    "revision": "4b2c5078ba9edf9090655c9e78f0d2b7",
+    "url": "/static/media/desktop.4b2c5078.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "fe888a1925fabb7dd199d2070a66a4e3",
+    "url": "/static/media/economy.fe888a19.png"
+  },
+  {
+    "revision": "caed892b2a0b817a8e275ae29638edaf",
+    "url": "/static/media/education.caed892b.svg"
+  },
+  {
+    "revision": "01fe24288abe2b0fe026d1ee4a48d74b",
+    "url": "/static/media/emotelabs.01fe2428.png"
+  },
+  {
+    "revision": "85d6957f6a0d678d1ac3932e5be4fcb2",
+    "url": "/static/media/experience.85d6957f.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "a73310ea0ad8aebedb65b3106024427a",
+    "url": "/static/media/feelingProud.a73310ea.svg"
+  },
+  {
+    "revision": "589bec7d632ecff9672dc4da97897bbd",
+    "url": "/static/media/fullstack.589bec7d.svg"
+  },
+  {
+    "revision": "e64de169a4222787bc49f3706282ef96",
+    "url": "/static/media/github_logo.e64de169.png"
+  },
+  {
+    "revision": "9dbc79dab48422c821c75bbfc21165ec",
+    "url": "/static/media/gmail.9dbc79da.svg"
+  },
+  {
+    "revision": "086132a5c0497bcc5ad58d82885b4f84",
+    "url": "/static/media/graph1.086132a5.png"
+  },
+  {
+    "revision": "3c221f68dff18a9a01e2a9cc2ff8f311",
+    "url": "/static/media/graph2.3c221f68.png"
+  },
+  {
+    "revision": "c57b18a664defe8575dc96c1fc9716d6",
+    "url": "/static/media/itr1.c57b18a6.png"
+  },
+  {
+    "revision": "c2cc820bc18dfe04dec0326759169178",
+    "url": "/static/media/itr2.c2cc820b.png"
+  },
+  {
+    "revision": "aab1e5e0bef4c341801fb1a2f3c64d2e",
+    "url": "/static/media/itr3.aab1e5e0.jpeg"
+  },
+  {
+    "revision": "01302fb203862604abfddea752c1310d",
+    "url": "/static/media/itr4.01302fb2.png"
+  },
+  {
+    "revision": "61eb777065649d641c3d473381b53ad5",
+    "url": "/static/media/itr5.61eb7770.png"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "10f10fe38d3aa2d9439b3610716a9de0",
+    "url": "/static/media/minimotorways.10f10fe3.png"
+  },
+  {
+    "revision": "1c75239daf39f0d8034e178e28140d89",
+    "url": "/static/media/mockup.1c75239d.png"
+  },
+  {
+    "revision": "cc88e3fa4468e01387d9ee8357ddf7d3",
+    "url": "/static/media/motivation.cc88e3fa.png"
+  },
+  {
+    "revision": "396b2ae21858d70d2d69d032c1b56ca4",
+    "url": "/static/media/palette.396b2ae2.png"
+  },
+  {
+    "revision": "7ace0cc9663b1da79689f411b13ba142",
+    "url": "/static/media/planetCoaster.7ace0cc9.png"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "f7964c6bf45e77547f1e5aae1e1dede0",
+    "url": "/static/media/projects_image.f7964c6b.svg"
+  },
+  {
+    "revision": "56ca64f9e456aabcff7d101f22a5bcfd",
+    "url": "/static/media/shortcut.56ca64f9.png"
+  },
+  {
+    "revision": "53b571dfd042d1ef1e6d9030257af8b6",
+    "url": "/static/media/sims4.53b571df.png"
+  },
+  {
+    "revision": "dd7c25350a6079b2a48c4ea2f0a746a7",
+    "url": "/static/media/studioLogo.dd7c2535.png"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "/static/media/talksCardBackAlt.b48e86c9.svg"
+  },
+  {
+    "revision": "b8f2967045668494f2ee1924a3d2b3cc",
+    "url": "/static/media/taskathonLogo.b8f29670.svg"
+  },
+  {
+    "revision": "045bc4b5032a558bdbc6e76b1ac5e188",
+    "url": "/static/media/twitter.045bc4b5.svg"
+  },
+  {
+    "revision": "c45186797876af37bf1c8d568bcb920a",
+    "url": "/static/media/ucsdLogo.c4518679.png"
+  },
+  {
+    "revision": "67a6a11c47edafb87cf24da66afe3dca",
+    "url": "/static/media/ui_ux_design.67a6a11c.svg"
+  },
+  {
+    "revision": "2bff6a565d6036e620d00a20d92f14cb",
+    "url": "/static/media/weatherHubLogo.2bff6a56.svg"
+  }
+]);
